a14c074f22daba79b61b3a6dbef91d016c73ba80
npee <jmc4664@gmail.com>
2022-07-13 01:53:47 +0900

    simple item entity 생성

---
+++ src/main/java/io/npee/spring/validation/domain/Item.java
@@ -0,0 +1,23 @@
+package io.npee.spring.validation.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Integer price;
+}
