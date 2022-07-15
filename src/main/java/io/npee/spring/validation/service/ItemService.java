6a06a9847ec658e98084e5f97eefda0628586020
npee <jmc4664@gmail.com>
2022-07-15 09:49:07 +0900

    서비스 로직 생성

---
+++ src/main/java/io/npee/spring/validation/service/ItemService.java
@@ -0,0 +1,7 @@
+package io.npee.spring.validation.service;
+
+import io.npee.spring.validation.domain.Item;
+
+public interface ItemService {
+    Item createItem(Item item);
+}
---
+++ src/main/java/io/npee/spring/validation/service/ItemServiceImpl.java
@@ -0,0 +1,16 @@
+package io.npee.spring.validation.service;
+
+import io.npee.spring.validation.domain.Item;
+import io.npee.spring.validation.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Override
+    public Item createItem(Item item) {
+        return itemRepository.save(item);
+    }
+}
