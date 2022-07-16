53f5d5bc89c01fde2816dc12a47a666d42059450
npee <jmc4664@gmail.com>
2022-07-16 17:14:14 +0900

    테스트에 objectmapper, assertions 사용

--- src/test/java/io/npee/spring/validation/controller/ItemControllerTest.java
+++ src/test/java/io/npee/spring/validation/controller/ItemControllerTest.java
@@ -5,27 +5,39 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.npee.spring.validation.domain.Item;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+@Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
 class ItemControllerTest {
 
     @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
     void addItemTest() throws Exception{
-        this.mockMvc.perform(post("/api/v1/item")
-                                 .contentType(MediaType.APPLICATION_JSON)
-                                 .content("{\"name\":\"itemA\",\"price\":\"20000\"}"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
+        Item item = new Item("itemA", 20000);
+        MvcResult mvcResult = this.mockMvc.perform(post("/api/v1/item")
+                                                       .contentType(MediaType.APPLICATION_JSON)
+                                                       .content(objectMapper.writeValueAsString(item)))
+                                          .andDo(print())
+                                          .andExpect(status().isOk())
+                                          .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        Item responseItem = objectMapper.readValue(contentAsString, Item.class);
+        assertEquals(item.getName(), responseItem.getName());
+        assertEquals(item.getPrice(), responseItem.getPrice());
     }
 }
